694dc654b978298ffe48c49052f56a91ed4529ca
Michael Kamenev <dlyapicem@gmail.com>
2021-09-01 05:52:09 +0300

    create base

---
+++ base/base.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    """Methods for working with webdriver."""
+    def __init__(self, browser):
+        self.browser = browser
+        self.base_url = "https://gmail.com/"
+
+    def find_element(self, locator, time=10):
+        """Find element."""
+        return WebDriverWait(self.browser, time).until(EC.presence_of_element_located(locator),
+                                                       message=f"Can't find element by locator {locator}")
+
+    def find_elements(self, locator, time=10):
+        """Find elements."""
+        return WebDriverWait(self.browser, time).until(EC.presence_of_all_elements_located(locator),
+                                                       message=f"Can't find elements by locator {locator}")
+
+    def go_to_site(self):
+        """Load page ."""
+        return self.browser.get(self.base_url)
+
+    def title(self):
+        """Get title."""
+        url = self.browser.current_url
+        WebDriverWait(self.browser, 10).until(EC.url_changes(url))
+        return self.browser.title
