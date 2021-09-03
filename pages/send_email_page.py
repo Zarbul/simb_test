b90dd4047ab0c086bdc961d724262697b372aa77
Michael Kamenev <dlyapicem@gmail.com>
2021-09-04 01:52:53 +0300

    fix imports, rename class

--- pages/send_email_page.py
+++ pages/send_email_page.py
@@ -1,9 +1,6 @@
-import time
-
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-from base.base import BasePage
+from .base import BasePage
 
 
 class SendLocators:
@@ -12,11 +9,12 @@ class SendLocators:
     LOCATOR_TO = (By.NAME, 'to')
     LOCATOR_TITLE = (By.NAME, 'subjectbox')
     LOCATOR_TEXT = (By.CSS_SELECTOR, 'div[aria-label="Тело письма"]')
+    LOCATOR_SEND_BUTTON = (By.XPATH, '//div[text()="Отправить"]')
 
 
-class SendHelper(BasePage):
+class SendEmailHelper(BasePage):
     """Create and send email."""
-    def write_button(self):
+    def click_on_write_button(self):
         """Push button 'Write'."""
         button = self.find_element(SendLocators.LOCATOR_WRITE_BUTTON)
         button.click()
@@ -30,4 +28,7 @@ class SendHelper(BasePage):
         topic_email.send_keys(title)
 
         text_email = self.find_element(SendLocators.LOCATOR_TEXT)
-        text_email.send_keys(text, Keys.CONTROL, Keys.ENTER)
+        text_email.send_keys(text)
+
+        send_button = self.find_element(SendLocators.LOCATOR_SEND_BUTTON)
+        send_button.click()
